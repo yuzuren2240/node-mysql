454f92c7d8a607ab3af2b55a14fbac1a3d7e159a
yuzuren2240 <j2240@matsue-ct.ac.jp>
2025-07-03 05:41:05 +0000

    susunda

---
+++ knexfile.js
@@ -0,0 +1,45 @@
+
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: "mysql",
+    connection: {
+      database: "todo_app",
+      user: "root",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+  },
+
+  staging: {
+    client: "mysql",
+    connection: {
+      database: "todo_app",
+      user: "root",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+  },
+
+  production: {
+    client: "mysql",
+    connection: {
+      database: "todo_app",
+      user: "root",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+  }
+
+};
--- routes/index.js
+++ routes/index.js
@@ -1,47 +1,41 @@
 const express = require('express');
 const router = express.Router();
-const mysql = require('mysql');
+const knex = require('../db/knex');
 
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'todo_app'
-});
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  connection.query(
-    `select * from tasks;`,
-    (error, results) => {
-      console.log(error);
+router.get('/', function (req, res, next) {
+  knex("tasks")
+    .select("*")
+    .then(function (results) {
       console.log(results);
       res.render('index', {
         title: 'ToDo App',
         todos: results,
       });
-    }
-  );
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render('index', {
+        title: 'ToDo App',
+      });
+    });
 });
-  
 
-router.post('/', function(req, res, next) {
- connection.connect((err) => {
-    if (err) {
-      console.log('error connecting: ' + err.stack);
-      return
-    }
-    console.log('success');
-  });
+router.post('/', function (req, res, next) {
   const todo = req.body.add;
-  connection.query(
-    `insert into tasks (user_id, content) values (1, '${todo}');`,
-    (error, results) => {
-      console.log(error);
-      res.redirect('/');
-    }
-  );
+  knex("tasks")
+    .insert({user_id: 1, content: todo})
+    .then(function () {
+      res.redirect('/')
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render('index', {
+        title: 'ToDo App',
+      });
+    });
 });
+
+router.use('/signup', require('./signup'));
+router.use('/signin', require('./signin'));
 
 module.exports = router;
